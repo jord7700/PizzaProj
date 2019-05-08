8c059204208ff413d1051b6c116ca761905068be
Nicholas Braniff <nicholas.braniff@live.com>
2019-05-07 23:32:28 -0400

    JQuery integration for php files
    - Added test.html for the html test JQuery page
    - Added test.js for the js test JQuery
    - Added Query.php for integration into JQuery with current databaseFunctions.php code

---
+++ query.php
@@ -0,0 +1,56 @@
+<?php
+	include "databaseFunctions.php";
+	$username = "";
+	$table = "";
+	$col = "";
+	$gsa = "";
+	$output = "";
+
+	if(isset($_GET['username'])){
+		$username = $_GET['username'];
+	}
+	
+	if(isset($_GET['table'])){
+		$table = $_GET['table'];
+	}
+	
+	if(isset($_GET['col'])){
+		$col = $_GET['col'];
+	}
+	
+	if(isset($_GET['type'])){
+		$type = $_GET['type'];
+	}
+	
+	if($type == "get"){
+		addUser($username);
+		if($table == "upgrades"){
+			$output = getFromUpgrades($username, $col);
+		}
+		if($table == "workers"){
+			$output = getFromWorkers($username, $col);
+		}
+		if($table == "stats"){
+			$output = getFromStats($username, $col);
+		}
+		echo $output;
+	}
+	
+	if($type == "post"){
+		addUser($username);
+		$value = "";
+		if(isset($_GET['value'])){
+			$value = $_GET['value'];
+		}		
+		if($table == "upgrades"){
+			$output = setUpgrades($username, $col, $value);
+		}
+		if($table == "workers"){
+			$output = setWorkers($username, $col, $value);
+		}
+		if($table == "stats"){
+			$output = setStats($username, $col, $value);
+		}
+		echo $output;
+	}
+?>
---
+++ test.js
@@ -0,0 +1,36 @@
+//$.get("databaseFunction.php", function(data, status){
+//      alert("Data: " + data + "\nStatus: " + status);
+//});
+
+$(document).ready(function(){
+	$("input#get").click(function(){
+		var name = $("input#name").val();
+		var table = $("input#table").val();
+		var col = $("input#col").val();
+		var type = "get";
+		$.get("query.php", {
+			username: name,
+			table: table,
+			col: col,
+			type: type
+		}, function(data, status) {
+			$("#test").html(data);
+		});
+	});
+	$("input#post").click(function(){
+		var name = $("input#name").val();
+		var table = $("input#table").val();
+		var col = $("input#col").val();
+		var value = $("input#value").val();
+		var type = "post";
+		$.get("query.php", {
+			username: name,
+			table: table,
+			col: col,
+			type: type,
+			value: value
+		}, function(data, status) {
+			$("#test").html(data);
+		});
+	});
+});
